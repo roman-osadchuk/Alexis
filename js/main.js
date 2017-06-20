a9db9498e03b49b055164c5d9ca4de6ebaab038d
Roman Osadchuk <uaromik@gmail.com>
2017-06-20 11:57:46 +0300

    First commit, layout completed

---
+++ js/main.js
@@ -0,0 +1,70 @@
+(function() {
+
+    (function(){
+        
+        $("#toggle_nav").click(function(){
+            $(".navigation > ul").slideToggle("normal");
+        });
+
+        $(window).resize(function(){
+            
+            if($(window).width() > 740){
+                $(".navigation > ul").css("display", "flex");
+            }else{
+                $(".navigation > ul").css("display", "none");
+            }
+        });
+        
+    })();
+    
+    (function(){
+        
+        $(".navigation > ul > li > a").each(function(){
+            $(this).click(function(e){
+                e.preventDefault();
+
+                if($(this).text() == "About Us"){
+                    $('html').animate({
+                        scrollTop: $(".container_3").offset().top + $(window).height() / 2.5
+                    });
+                }else if($(this).text() == "Our Focus"){
+                    $('html').animate({
+                        scrollTop: $(".container_4").offset().top
+                    });
+                }else if($(this).text() == "Portfolio"){
+                    $('html').animate({
+                        scrollTop: $(".container_6").offset().top
+                    });
+                }else if($(this).text() == "Our Team"){
+                    $('html').animate({
+                        scrollTop: $(".container_7").offset().top
+                    });
+                }else if($(this).text() == "Work Steps"){
+                    $('html').animate({
+                        scrollTop: $(".container_9").offset().top
+                    });
+                }else if($(this).text() == "Pricing"){
+                    $('html').animate({
+                        scrollTop: $(".container_10").offset().top
+                    });
+                }else if($(this).text() == "Testimonials"){
+                    $('html').animate({
+                        scrollTop: $(".container_11").offset().top
+                    });
+                }else if($(this).text() == "Blog"){
+                    $('html').animate({
+                        scrollTop: $(".container_13").offset().top
+                    });
+                }else if($(this).text() == "Contact"){
+                    $('html').animate({
+                        scrollTop: $(".container_15").offset().top
+                    });
+                }
+            });
+        });
+
+    })();
+    
+
+    
+})();
